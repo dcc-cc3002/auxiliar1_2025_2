b1ef42dfcd4a0c7bfaf76b38bfdd3f8fbc1e17cb
nachoroas <mrojascornejo@gmail.com>
2025-08-07 19:21:52 -0400

    Initial Commit

---
+++ src/main/scala/Yapper.scala
@@ -0,0 +1,12 @@
+object Yapper {
+
+  val greeting:String=""
+  val farewell:String=""
+  def main(args: Array[String]): Unit = {
+    println("Hello my fellow memeros")
+  }
+
+  def say(something:String):Unit={
+
+  }
+}
