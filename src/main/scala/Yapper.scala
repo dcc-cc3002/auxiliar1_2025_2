6d80f9e91f824da6c3c1600ff5b205042d0a2f00
AVargas0420 <alejandro.vargas.o@ug.uchile.cl>
2025-08-08 17:23:58 -0400

    Hola, soy Homero Chino

--- src/main/scala/Yapper.scala
+++ src/main/scala/Yapper.scala
@@ -3,7 +3,7 @@ object Yapper {
   val greeting:String=""
   val farewell:String=""
   def main(args: Array[String]): Unit = {
-    println("Hola, soy Homero my fellow memeros")
+    println("Hola, soy Homero Chino")
   }
 
   def say(something:String):Unit={
