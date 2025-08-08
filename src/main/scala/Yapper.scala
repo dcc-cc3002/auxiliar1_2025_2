21be5da109cd26684a74d6a12810e2cc49ae6710
ElVenJi <benjaminalvaradoguerra@gmail.com>
2025-08-08 17:22:34 -0400

    wasaaa

--- src/main/scala/Yapper.scala
+++ src/main/scala/Yapper.scala
@@ -3,10 +3,10 @@ object Yapper {
   val greeting:String=""
   val farewell:String=""
   def main(args: Array[String]): Unit = {
-    println("Hello my fellow memeros")
+    println("WASAAAAAAAAAAAAa")
   }
 
   def say(something:String):Unit={
 
   }
-}
+}   
