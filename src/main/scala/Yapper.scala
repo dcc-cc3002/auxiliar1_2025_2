6dac9331af4cab06121305663144fb4a1e20ef58
Hector Rodriguez <rohtor204@gmail.com>
2025-08-08 17:24:19 -0400

    Chamo tu sabe lo que es un completo... Le ponen aguacate al perro caliente

--- src/main/scala/Yapper.scala
+++ src/main/scala/Yapper.scala
@@ -3,7 +3,11 @@ object Yapper {
   val greeting:String=""
   val farewell:String=""
   def main(args: Array[String]): Unit = {
-    println("Hello my fellow memeros")
+    println("When haces tus momos en Yapper")
+    println("El futuro es hoy oiste viejo?")
+    println("Pero te terminan baneando")
+    println("Oh mi lente de contacto!")
+    println("JIJIJIJJIJIJI")
   }
 
   def say(something:String):Unit={
