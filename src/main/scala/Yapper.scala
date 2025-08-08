4721e0f90ffca55d57112d8f33fad66ea554deac
AVargas0420 <alejandro.vargas.o@ug.uchile.cl>
2025-08-08 17:33:35 -0400

    Fix: Changed soy to fui

--- src/main/scala/Yapper.scala
+++ src/main/scala/Yapper.scala
@@ -3,7 +3,7 @@ object Yapper {
   val greeting:String=""
   val farewell:String=""
   def main(args: Array[String]): Unit = {
-    println("Hola, soy Homero Chino")
+    println("Hola, fui Homero Chino")
   }
 
   def say(something:String):Unit={
