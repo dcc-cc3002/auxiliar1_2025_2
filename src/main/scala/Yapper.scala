c73f145751706f41a7b543ef4465edc5ed6799cd
Julio Yáñez <julio.yanez@ug.uchile.cl>
2025-08-08 17:29:11 -0400

    Docs: Un poco de documentación para Yapper y ajustes estéticos al README

--- src/main/scala/Yapper.scala
+++ src/main/scala/Yapper.scala
@@ -1,12 +1,12 @@
-object Yapper {
+object Yapper { //Yapper es un objeto
 
-  val greeting:String=""
-  val farewell:String=""
-  def main(args: Array[String]): Unit = {
-    println("Hello my fellow memeros")
+  val greeting:String="Wena cabros" // Define el saludo del Yapper
+  val farewell:String="Hasta luego" // Define la despedidad del Yapper
+  def main(args: Array[String]): Unit = { //Función main del objeto Yapper
+    println("Hello my fellow memeros") // Lo priero que dice el Yapper
   }
 
   def say(something:String):Unit={
-
+    // Puede definirse un diálogo que puede hacer el Yapper
   }
 }
